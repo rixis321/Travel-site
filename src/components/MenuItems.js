58b7a148a9b442e1397cdd5b0cb83ff23dee2d84
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-12-06 18:10:35 +0100

    Navbar done

---
+++ src/components/MenuItems.js
@@ -0,0 +1,31 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "/",
+    cName: "nav-links",
+    icon: "fa-sharp fa-solid fa-house",
+  },
+  {
+    title: "About",
+    url: "/about",
+    cName: "nav-links",
+    icon: "fa-sharp fa-solid fa-circle-info",
+  },
+  {
+    title: "Service",
+    url: "/service",
+    cName: "nav-links",
+    icon: "fa-sharp fa-solid fa-briefcase",
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+    cName: "nav-links",
+    icon: "fa-sharp fa-solid fa-address-book",
+  },
+  {
+    title: "Sign-up",
+    url: "/signup",
+    cName: "nav-links-mobile",
+  },
+];
