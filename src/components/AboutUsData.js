fd0ed503a5e3bc0b6c651e70acd33cefdfa62687
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-12-08 05:26:45 +0100

    AboutUsData component done

---
+++ src/components/AboutUsData.js
@@ -0,0 +1,30 @@
+import "./AboutUsData.css";
+
+const AboutUsData = () => {
+  return (
+    <div className="about-container">
+      <h1>Our History</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+        deleniti, distinctio dolore doloremque facilis natus nobis sit tenetur
+        veritatis vitae. Ad amet at corporis debitis dolores error esse, eum
+        exercitationem impedit in iure iusto obcaecati placeat quam qui quia
+        quo? Aliquam consequatur labore nobis possimus praesentium recusandae,
+        saepe similique soluta!
+      </p>
+      <h1>Our Mission</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias amet
+        aut dolores error eum, harum illo incidunt obcaecati omnis, rerum vel,
+        voluptas. Facere fugit minima perspiciatis sed unde voluptatibus?
+      </p>
+      <h1>Our Vision</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab architecto
+        iure magnam reiciendis, sit vel.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUsData;
