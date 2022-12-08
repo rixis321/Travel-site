f2c35e5c38b8c38fb684ad7c6f85c945083b45f2
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-12-08 05:02:55 +0100

    Added trip component

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutImg from "../../images/image-6.jpg";
 import Footer from "../components/Footer";
+import Trip from "../components/Trip";
 
 function Service() {
   return (
@@ -14,6 +15,7 @@ function Service() {
         url="/"
         btnClass="hide"
       />
+      <Trip />
       <Footer />
     </>
   );
