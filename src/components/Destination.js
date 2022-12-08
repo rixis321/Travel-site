5b169350f8c7dd7ff4d639d949320850630ff654
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-12-08 04:28:52 +0100

    Destination component done

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,8 +1,9 @@
 import "./DestinationStyles.css";
+import DestinationData from "./DestinationData";
 import pict1 from "../../images/volcan.jpg";
 import pict2 from "../../images/volcan2.jpg";
-import pict3 from "../../images/volcan3.jpg";
-import pict4 from "../../images/volcan4.jpg";
+import pict3 from "../../images/image-7.jpg";
+import pict4 from "../../images/image-1.jpg";
 
 const Destination = () => {
   return (
@@ -10,22 +11,29 @@ const Destination = () => {
       <h1>Popular Destinations</h1>
       <p>Tours gives you the opportunity to see a lot, within a time frame. </p>
 
-      <div className="first-des">
-        <div className="des-text">
-          <h2>Taal Volcano, Batangas</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
+      <DestinationData
+        className="first-des"
+        heading="Taal Volcano, Batangas"
+        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
             iusto minima, perferendis perspiciatis quasi quisquam quod sed sit
             ut. Aliquam animi at, commodi, consequuntur eveniet excepturi
             facilis magnam mollitia nam neque numquam reprehenderit sint
-            tenetur.
-          </p>
-        </div>
-        <div className="image">
-          <img alt="volcan" src={pict1} />
-          <img alt="volcan" src={pict2} />
-        </div>
-      </div>
+            tenetur."
+        img1={pict1}
+        img2={pict2}
+      />
+
+      <DestinationData
+        className="first-des-reverse"
+        heading="Lorem ipsum dolor sit amet."
+        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
+            iusto minima, perferendis perspiciatis quasi quisquam quod sed sit
+            ut. Aliquam animi at, commodi, consequuntur eveniet excepturi
+            facilis magnam mollitia nam neque numquam reprehenderit sint
+            tenetur."
+        img1={pict3}
+        img2={pict4}
+      />
     </div>
   );
 };
