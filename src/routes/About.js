f033b342c3c4ef35c00fb9a935e298147013d7df
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-12-08 05:26:23 +0100

    Added AboutUsData component

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutImg from "../../images/image-2.jpg";
 import Footer from "../components/Footer";
+import AboutUsData from "../components/AboutUsData";
 
 function About() {
   return (
@@ -14,6 +15,7 @@ function About() {
         url="/"
         btnClass="hide"
       />
+      <AboutUsData />
       <Footer />
     </>
   );
